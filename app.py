8fca4587cfec9162d8bf9f97006c57d0529cee71
Xeliia <axelaaronarceleta@gmail.com>
2025-05-01 03:56:02 +0800

    Finished Login and Register System

--- app.py
+++ app.py
@@ -10,14 +10,13 @@ app = Flask(__name__)
 
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+app.config["UPLOAD_FOLDER"] = "static/uploads"
 Session(app)
 
-sql.connect('data.db')
-
 def get_db_connection():
-    conn = sql.connect('finance.db')
-    conn.row_factory = sql.Row
-    return conn
+    connection = sql.connect('data.db')
+    connection.row_factory = sql.Row
+    return connection
 
 @app.after_request
 def after_request(response):
@@ -30,8 +29,8 @@ def after_request(response):
 @app.route("/")
 @login_required
 def index():
-    """Show portfolio of stocks"""
-
+    """Homepage"""
+    
     return render_template("index.html")
 
 
@@ -44,18 +43,31 @@ def buy():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    """Log user in"""
-
-    # Forget any user_id
     session.clear()
+    if request.method == "POST":
+        if not request.form.get("username"):
+            flash("Username is required", "danger")
+            return redirect("/login")
+        if not request.form.get("password"):
+            flash("Please Provide the Password", "danger")
+            return redirect("/login")
 
-    return render_template("login.html")
+        conn = get_db_connection()
+        rows = conn.execute("SELECT * FROM users where username = ?", request.form.get("username"))
+        if len(rows) != 1 or not check_password_hash (
+            rows[0]["password"], request.form.get("password")
+        ):
+            flash("Invalid Username or Password", "danger")
+            return redirect("/login")
+
+        session["user_id"] = rows[0]["id"]
+        return redirect("/")
+    else:
+        return render_template("login.html")
 
 
 @app.route("/logout")
 def logout():
-    """Log user out"""
-
     # Forget any user_id
     session.clear()
 
@@ -73,7 +85,38 @@ def quote():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    """Register user"""
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+        profile_pic = request.files.get("profile_pic")
+
+        if not username or not password or not confirmation:
+            flash("All fields are required except profile picture", "danger")
+            return redirect("/register")
+        connection = get_db_connection()
+        cursor = connection.cursor()
+        
+        user = cursor.execute("SELECT * FROM users WHERE username = ?", username)
+        if user:
+            flash("Username already exists", "danger")
+            return redirect("/register")
+        
+        hash_pass = generate_password_hash(password)
+        
+        filename = "placeholder.png"
+        if profile_pic and profile_pic.filename != "":
+            filename = f"{username}_{profile_pic.filename}"
+            filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+            profile_pic.save(filepath)
+
+        cursor.execute("INSER INTO users (username, hash, profile_pic) VALUES (?, ?, ?)", username, hash_pass, filename)
+        
+        connection.commit()
+        connection.close()
+
+        flash("User Created Successfully", "success")
+        return redirect("/login")
 
     return render_template("register.html")
 
@@ -82,7 +125,7 @@ def register():
 @login_required
 def sell():
 
-    return render_template("sell.html", symbols=stocks)
+    return render_template("sell.html")
 
 
 @app.route("/change-password", methods=["GET", "POST"])
